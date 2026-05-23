b108edb947c415c4cec191bc99c78ba904cc6707
tajagn01 <trgarala@gmail.com>
2026-05-23 09:03:50 +0530

    add initial implementation of add3number function

---
+++ javamarquee/function/add3number.java
@@ -0,0 +1,12 @@
+package javamarquee.function;
+
+public class add3number {
+
+    public static  int add3number(int a,int b,int c){
+        return a+b+c;
+    }
+    public static void main(String[] args) {
+      
+        System.out.print("The answer is : "+add3number(2,3,4));   
+    }
+}
