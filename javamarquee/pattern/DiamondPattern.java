ac7b17c67140ee5a9ff1f62bc2a0c203512222cb
tajagn01 <trgarala@gmail.com>
2026-05-23 08:18:15 +0530

    adding some pattern quetion: dinmond,hollowsqure,holowtringle,staircasepattern..etc

---
+++ javamarquee/pattern/DiamondPattern.java
@@ -0,0 +1,29 @@
+package javamarquee.pattern;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int N = 5;
+        for(int i= 1 ; i <= N ;i++){
+            for(int j = 1 ; j < N - i ; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1 ; j <= 2*i - 1 ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1 ; i <= N ; i++){
+           
+            for(int j = 1 ; j <= i - 1 ; j++){
+                System.out.print(" ");
+            }
+           
+            for(int j = 1 ; j <= 2 * (N - i) + 1; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ javamarquee/pattern/StairCasePattern.java
@@ -0,0 +1,21 @@
+package javamarquee.pattern;
+
+public class StairCasePattern {
+    public static void main(String[] args) {
+        int n = 5;
+        int stars = 2;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+
+            if (i % 2 == 0) {
+                stars += 2;
+            }
+        }
+    }
+}
